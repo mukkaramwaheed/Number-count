9d6b156d0c0428bb72e4972d4fb00524055d3a67
Mukkaram waheed <mukkaramwaheed@gmail.com>
2022-06-14 13:27:27 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import "./App.css";
+import { useState } from "react";
+import Papa from "papaparse";
+
+function App() {
+  // State to store parsed data
+  const [parsedData, setParsedData] = useState([]);
+
+  //State to store table Column name
+  const [tableRows, setTableRows] = useState([]);
+
+  //State to store the values
+  const [values, setValues] = useState([]);
+
+  const changeHandler = (event) => {
+    Papa.parse(event.target.files[0], {
+      header: true,
+      skipEmptyLines: true,
+      complete: function (results) {
+        console.log(results.meta.fields);
+      }
+    });
+  };
+
+  return (
+    <div>
+      {/* File Uploader */}
+      <input
+        type="file"
+        name="file"
+        onChange={changeHandler}
+        accept=".csv"
+        style={{ display: "block", margin: "10px auto" }}
+      />
+    </div>
+  );
+}
+
+export default App;
